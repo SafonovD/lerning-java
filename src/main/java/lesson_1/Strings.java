64e8946b85da864fbbc0b9495c83f4837f510fab
Dmitriy Safonov <d.lexed@gmail.com>
2023-06-30 12:29:53 +0300

    lesson 1

---
+++ src/main/java/lesson_1/Strings.java
@@ -0,0 +1,15 @@
+package lesson_1;
+
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String str = "Привет мир!!";
+        String str1 = "Это я";
+
+        System.out.println(str+" "+str1);
+    }
+
+
+
+}
---
+++ src/main/java/lesson_1/Variable.java
@@ -0,0 +1,20 @@
+package lesson_1;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        byte myByte = 1;  // -128 до 127
+        short myShort = 1;
+        int myInteger = 1;
+        long myLong = 2l;
+        char myChar = 'a';
+        float myFloat = 12.2f;
+        double myDouble = 23.2;
+        boolean myBoolean = true;
+
+        System.out.println( myByte + "," + myShort + "," + myInteger + "," + myLong + "," +
+                myChar + "," + myFloat + "," + myDouble + "," + myBoolean);
+
+    }
+
+}
